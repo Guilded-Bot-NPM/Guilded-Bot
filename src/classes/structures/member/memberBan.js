b2cd42dd64f385ed8b77f2d9267a38c9e4dee042
MDCDEV <me@mdcdev.me>
2022-10-08 01:57:12 -0500

    fix: Fix Ban structure

--- src/classes/structures/member/memberBan.js
+++ src/classes/structures/member/memberBan.js
@@ -3,15 +3,32 @@ const UserSummary = require('../user/userSummary.js');
 class MemberBan {
     /**
      * MemberBan constructor
+     * @param {String} serverId The server id of the member ban
      * @param {Object} banData The ban data
      * @param {Object} banData.serverMemberBan The server member ban data
-     * @param {Object} banData.serverMemberBan.user The user who was banned
+     * @param {UserSummary} banData.serverMemberBan.user The user who was banned
      * @param {String | undefined} banData.serverMemberBan.reason The reason for the ban
      * @param {String} banData.serverMemberBan.bannedAt The date the user was banned
      * @param {String} banData.serverMemberBan.bannedBy The user who banned the user
      * @returns {MemberBan<UserSummary, String | undefined, String, String>} The user who was banned, the reason for the ban, the date the user was banned, the user who banned the user
      */
-    constructor(banData) {
+    constructor(banData, client) {
+        /**
+         * The server id of the member ban
+         * @type {String}
+         * @readonly
+         * @private
+         */
+        this.serverId = banData.serverId;
+
+        /**
+         * The client
+         * @type {Client}
+         * @readonly
+         * @private
+         */
+        this.client = client;
+
         /**
          * The UserSummary of the user who was banned
          * @type {UserSummary} 
